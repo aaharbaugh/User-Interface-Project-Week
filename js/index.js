48cce4d4598c30d8c7a3e0ce1676be52ddb3c8b1
aaharbaugh <aaronmharbaugh@gmail.com>
2019-02-16 13:39:47 -0600

    removed 1st attempted at navbar js, starting over, currently clickable, toggles links

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // JS goes here
+
+let navBarButton = document.querySelector('.navbar-img');
+let topNavMenu = document.querySelector('.top-menu');
+let navBarLinks = document.querySelector('.links');
+
+navBarButton.addEventListener('mouseover', (event) => {
+    navBarButton.style.cursor = 'pointer';
+})
+
+navBarButton.addEventListener('click', (event) => {
+    navBarLinks.style.display = 'flex'
+})
