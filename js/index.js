96cbdaad8e3ee0a59d25976ada5e4e90399e1e5c
aaharbaugh <aaronmharbaugh@gmail.com>
2019-02-16 16:47:59 -0600

    finished service page tab selector

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 // JS goes here
 
+//nav top menu drop down code
 let navBarButton = document.querySelectorAll('.navbar-img');
 let topNavMenu = document.querySelector('.top-menu');
 let navBarLinks = document.querySelector('.links');
@@ -20,3 +21,53 @@ navBarButton.forEach(img => img.addEventListener('click', (event) => {
     
     }
 }))
+
+//code for tabs on service page.
+
+class TabLink {
+    constructor(tabElement){
+        this.tabElement = tabElement;
+        this.tabData = this.tabElement.dataset.tab;
+
+    this.card = document.querySelectorAll(`div[data-tab="${this.tabData}"]`);
+
+    this.card = Array.from(this.card).map(card => new TabCard(card));
+
+    this.tabElement.addEventListener('click', (event) => {
+        this.selectTab();
+    });
+
+    }
+
+    selectTab(){
+
+        const tabs = document.querySelectorAll('.tab-button');
+
+        tabs.forEach(tab => tab.classList.remove('selected'))
+
+        this.tabElement.classList.add('selected');
+
+        const tabRow = document.querySelectorAll('.tab-row');
+
+            tabRow.forEach(row => row.style.display = 'none');
+
+        this.card.forEach(card => {
+            console.log(card)
+            card.selectCard()
+        });
+
+    }
+}
+
+class TabCard{
+    constructor(tabElement){
+        this.tabElement = tabElement;
+        console.log('yo')
+    }
+    selectCard(){
+        console.log('yo')
+        this.tabElement.style.display = "flex";
+    }
+}
+
+let tabs = document.querySelectorAll('.tab-button').forEach(tab => new TabLink(tab));
