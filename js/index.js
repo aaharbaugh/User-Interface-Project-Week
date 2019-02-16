a0e97b072546b9688b599d8aabad126ceec90a52
aaharbaugh <aaronmharbaugh@gmail.com>
2019-02-16 14:28:54 -0600

    finished nav bar click event to hide/show links & modify hamburger img

--- js/index.js
+++ js/index.js
@@ -1,13 +1,21 @@
 // JS goes here
 
-let navBarButton = document.querySelector('.navbar-img');
+let navBarButton = document.querySelectorAll('.navbar-img');
 let topNavMenu = document.querySelector('.top-menu');
 let navBarLinks = document.querySelector('.links');
 
-navBarButton.addEventListener('mouseover', (event) => {
-    navBarButton.style.cursor = 'pointer';
-})
+navBarButton.forEach(img => img.addEventListener('mouseover', (event) => {
+    img.style.cursor = 'pointer';
+}))
 
-navBarButton.addEventListener('click', (event) => {
-    navBarLinks.style.display = 'flex'
-})
+navBarButton.forEach(img => img.addEventListener('click', (event) => {
+    navBarLinks.classList.toggle('clicked');
+
+    navBarButton.forEach(img => img.classList.toggle('clicked'));
+    
+    if(topNavMenu.style.position === 'fixed'){
+        topNavMenu.style.position = 'absolute'
+    } else {
+        topNavMenu.style.position = 'fixed'
+    }
+}))
