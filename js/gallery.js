c272d111960672c3da0da51f7734ae34ad51200d
aaharbaugh <aaronmharbaugh@gmail.com>
2019-02-22 22:33:03 -0600

    final touches on gallery. image has overflow

--- js/gallery.js
+++ js/gallery.js
@@ -1,118 +1,97 @@
 const left = document.querySelector('.img-top-left')
 const right = document.querySelector('.img-bot-right')
-const shadow = document.querySelector('.image-container')
+const background = document.querySelector('.img-behind')
 
 const imgArray = [
-    "https://unsplash.it/600/360?image=1072",
-    "https://unsplash.it/600/360?image=1067",
-    "https://unsplash.it/600/360?image=1065",
-    "https://unsplash.it/600/360?image=1048",
-    "https://unsplash.it/600/360?image=534"
+    "https://unsplash.it/800/460?image=1072",
+    "https://unsplash.it/800/460?image=1067",
+    "https://unsplash.it/800/460?image=1065",
+    "https://unsplash.it/800/460?image=1048"
 ]
+
+//image preloader
+images = [];
+function preload() {
+    for (var i = 0; i < arguments.length; i++) {
+        images[i] = new Image();
+        images[i].src = preload.arguments[i];
+    }
+}
+preload(imgArray)
 
 let counter = 0;
 
 right.addEventListener('click', (event) => {
 
-    TweenMax.to(shadow, 0.2, {
-        opacity: 0,
-        ease: 'Power2.easeIn'
+    counter++
+    if(counter === 4){
+        counter = 0;
+    }
+
+    background.src = imgArray[counter];
+
+    TweenMax.to(right, 1, {
+        x: 900, 
+        opacity: 1
       })
-    TweenMax.to(right, 0.5, {
-        x: 300,
-        opacity: 0,
-        ease: 'Power2.easeIn'
-      })
-      TweenMax.to(left, 0.5, {
-        x: -300,
-        opacity: 0,
-        ease: 'Power2.easeIn',
-        delay: 0.15
+      TweenMax.to(left, 1, {
+        x: -900,
+        opacity: 1
       })
 
 
-      setTimeout(changeImgRight, 600)
-      setTimeout(tweenInRight, 700)
+      setTimeout(changeImg, 1000)
+      setTimeout(tweenInRight, 1010)
 })
 
 left.addEventListener('click', (event) => {
 
-    TweenMax.to(shadow, 0.2, {
-        opacity: 0,
-        ease: 'Power2.easeIn'
+    counter--
+    if(counter === -1){
+        counter = 3;
+    }
+
+    background.src = imgArray[counter];
+
+    TweenMax.to(right, 0.8, {
+        x: 900,
+        opacity: 1
       })
-    TweenMax.to(right, 0.5, {
-        x: 400,
-        opacity: 0,
-        ease: 'Power2.easeIn',
-        delay: 0.15
-      })
-      TweenMax.to(left, 0.5, {
-        x: -400,
-        opacity: 0,
-        ease: 'Power2.easeIn'
+      TweenMax.to(left, 0.8, {
+        x: -900,
+        opacity: 1
       })
 
       //function to change image, and then do final tween In, after timeouts. 
-      setTimeout(changeImgLeft, 600)
-      setTimeout(tweenInLeft, 700)
+      setTimeout(changeImg, 1000)
+      setTimeout(tweenInLeft, 1010)
 })
 
 //change image function via counter and array
-const changeImgRight = function() {
-    counter++
-    if(counter === 5){
-        counter = 0;
-    }
-    right.src = imgArray[counter];
-    left.src = imgArray[counter];
-}
-const changeImgLeft = function() {
-    counter--
-    if(counter === -1){
-        counter = 4;
-    }
-    right.src = imgArray[counter];
-    left.src = imgArray[counter];
-}
+const changeImg = function() {
 
+    right.src = imgArray[counter];
+    left.src = imgArray[counter];
+
+}
 //if clicking right, comes in a bit later
 const tweenInRight = function () {
 
-    TweenMax.to(shadow, 0.5, {
-        opacity: 1,
-        ease: 'Power2.easeIn',
-        delay: 0.15
+    TweenMax.to(right, 0, {
+        x: 0
       })
-    TweenMax.to(right, 0.5, {
-        x: 0,
-        opacity: 1,
-        ease: 'Power2.easeOut',
-        delay: 0.15
-      })
-      TweenMax.to(left, 0.5, {
-        x: 0,
-        opacity: 1,
-        ease: 'Power2.easeOut'
+      TweenMax.to(left, 0, {
+        x: 0
       })
 }
 
 //if clicking left, comes in a bit later
 const tweenInLeft = function () {
 
-    TweenMax.to(shadow, 0.5, {
-        opacity: 1,
-        ease: 'Power2.easeIn'
+    TweenMax.to(right, 0, {
+        x: 0
       })
-    TweenMax.to(right, 0.5, {
-        x: 0,
-        opacity: 1,
-        ease: 'Power2.easeOut'
-      })
-      TweenMax.to(left, 0.5, {
-        x: 0,
-        opacity: 1,
-        ease: 'Power2.easeOut',
-        delay: 0.15
+      TweenMax.to(left, 0, {
+        x: 0
       })
 }
